69960b47db95584a859dddae1d15b6c6d200dba1
Fabio Matricardi <fabio.matricardi@gmail.com>
2024-05-03 21:54:00 +0800

    Final version
    fixed ffmpeg loop, add Status messages and Audio Player

--- stWhisper.py
+++ stWhisper.py
@@ -18,7 +18,7 @@ def genRANstring(n):
 
 @st.cache_resource 
 def create_whisper():   
-    whisper = Whisper("models/ggml-base.en-q5_1.bin")
+    whisper = Whisper("models/ggml-small.en-q5_1.bin")
     return whisper
 
 @st.cache_resource
@@ -35,15 +35,16 @@ if "audiofile" not in st.session_state:
     st.session_state.audiofile = ''    
 
 def main():
-
     st.set_page_config(layout="wide", page_title="AI Whisper Transcriber")
     whisper = create_whisper()
-    st.write("# 🌇🏙️ Transcribe your Audio files with whisper.CPP")
-
+    st.write("# 🎙️✍️ Transcribe your Audio files with whisper.CPP\n\n\n")
+    st.markdown('\n---\n', unsafe_allow_html=True)
     st.sidebar.write("## Upload an audio file :gear:")
     file1=None
-    transcribe_btn = st.button('Start Audio AI', type='primary')
+    transcribe_btn = st.button('✨ **Start AI Magic**', type='primary')
+    st.markdown('\n\n')
     message1 = st.empty()
+    message11 = st.empty()
     message2 = st.empty()
     message3 = st.empty()
     audioplayer = st.empty()
@@ -58,8 +59,9 @@ def main():
             print(file1.name)
             if 'mp3' in file1.name:
                 print('The file is an MP3: starting ffmpeg')
+                message1.info(' Your Audio file is a MP3: we are going to convert it!',icon='⏳')
                 out = ffmpegconvert(file1.name)
-                message1.success('Audio encoded into WAV')
+                message11.success(' Audio file correcty encoded into WAV 16k Mono',icon='✅')
                 start = datetime.datetime.now()
                 print('Start transcribing...')
                 whisper.transcribe('temp.wav', 
@@ -68,15 +70,18 @@ def main():
                 delta = datetime.datetime.now() - start
                 st.session_state.gentime = f"**:green[{str(delta)}]**"
                 gentimetext.write(st.session_state.gentime)
-                message2.success('Audio transcripted by AI')
+                message2.success(' Audio transcribed by AI',icon='✅')
                 print('removing temp files...')
-                os.remove('temp.wav')
+                try:
+                    os.remove('temp.wav')
+                except:
+                    pass    
                 print('writing text file out...')
                 result = whisper.output(output_txt=True)
                 transcribed.write(result)
                 print('completed')
 
-            else:
+            else:             
                 start = datetime.datetime.now()
                 whisper.transcribe(file1.name, 
                                 diarize=False,
@@ -84,13 +89,22 @@ def main():
                 delta = datetime.datetime.now() - start
                 st.session_state.gentime = f"**:green[{str(delta)}]**" 
                 gentimetext.write(st.session_state.gentime)
-                message2.success('Audio transcripted by AI')                
+                message2.success(' Audio transcribed by AI',icon='✅')                
                 result = whisper.output(output_txt=True)
                 transcribed.write(result)
+                try:
+                    os.remove('temp.wav')
+                except:
+                    pass
 
     if  not file1:
-        message3.warning("Upload an audio file")
-            
+        message3.warning("  Upload an audio file", icon='⚠️')
+    if file1:
+        if 'mp3' in file1.name:
+            audioplayer.audio(file1.name, format="audio/mpeg")
+        else:
+            audioplayer.audio(file1.name, format="audio/wav")
+
 
 if __name__ == "__main__":
     main()
